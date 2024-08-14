cf00e3e70f26235c19f3c7b49d731c338a952398
balthazar-crew <balthazar@crew.brussels>
2024-08-14 12:15:28 +0200

    refactored Tracker.cs

--- Assets/Trackers/Tracker.cs
+++ Assets/Trackers/Tracker.cs
@@ -1,10 +1,7 @@
-using OscJack;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.XR;
 using Wave.Essence.Tracker;
 
 public class Tracker : MonoBehaviour
@@ -12,9 +9,10 @@ public class Tracker : MonoBehaviour
     public GameObject Canvas;
     public TMP_Text Name;
     public TMP_Text ID;
+    public TMP_Text Address;
+    public TMP_Text Status;
     public TMP_Text Bat;
 
-    public OscConnection oscConnection;
     public string TrackerRole;
 
     private TrackerManager trackerManager;
@@ -27,48 +25,116 @@ public class Tracker : MonoBehaviour
         this.InitTracker = initTracker;  
         trackerManager = TrackerManager.Instance;
         TrackerId = trackerId;
-        ID.text = trackerId.ToString();
-        Name.text = trackerManager.GetTrackerDeviceName(trackerId);
 
-        if (string.IsNullOrEmpty(TrackerRole))
-            TrackerRole = TrackerId.ToString();
+        if (ID != null )
+            ID.text = trackerId.ToString();
+        if (Name != null )
+            Name.text = trackerManager.GetTrackerDeviceName(trackerId);
 
         UpdateOSCAddress();
     }
 
     public string UpdateOSCAddress(string adr = null)
     {
+        if (string.IsNullOrEmpty(TrackerRole))
+            TrackerRole = TrackerId.ToString();
+
         if (adr == null)
             OSCAddress = "/" + InitTracker.oscAddress.Trim('/').Trim() + "/" + TrackerRole.Trim('/').Trim();
         else
             OSCAddress = adr;
 
+        if (Address != null)
+            Address.text = OSCAddress;
+
         return OSCAddress;
     }
 
-    void Update()
+    private bool ShowInfo
     {
-        if (trackerManager.IsTrackerConnected(TrackerId))
+        get
         {
-            Canvas.SetActive(true);
-            
-            transform.position = trackerManager.GetTrackerPosition(TrackerId);
-            transform.rotation = trackerManager.GetTrackerRotation(TrackerId);
-            Bat.text = "Battery " + trackerManager.GetTrackerBatteryLife(TrackerId).ToString("P1", CultureInfo.InvariantCulture);
-            foreach (TrackerButton button in (TrackerButton[])Enum.GetValues(typeof(TrackerButton)))
-                if (trackerManager.TrackerButtonPress(TrackerId, button))
-                    ID.text = button.ToString();
-
-            SendOSCMessage();
+            return _ShowInfo;
         }
-        else
-            Canvas.SetActive(false);
+        set
+        {
+            if (_ShowInfo != value)
+            {
+                _ShowInfo = value;
+                if (Canvas != null)
+                    Canvas.SetActive(value);
+            }
+        }
     }
+    private bool _ShowInfo;
 
+    private float BatteryValue
+    {
+        get
+        {
+            return _BatteryValue;
+        }
+        set
+        {
+            if (_BatteryValue != value)
+            {
+                _BatteryValue = value;
+                if (Bat != null)
+                    Bat.text = "Battery " + trackerManager.GetTrackerBatteryLife(TrackerId).ToString("P1", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+    private float _BatteryValue;
+
+    private InputTrackingState TrackingState
+    {
+        get
+        {
+            return _TrackingState;
+        }
+        set
+        {
+            if (_TrackingState != value)
+            {
+                _TrackingState = value;
+                if (Status != null)
+                {
+                    string pos = (value & InputTrackingState.Position) != 0 ? "green" : "red";
+                    string rot = (value & InputTrackingState.Rotation) != 0 ? "green" : "red";
+
+                    Status.richText = true;
+                    Status.text = $"<color=\"{pos}\">Position <color=\"{rot}\">Rotation";
+                }
+            }
+        }
+    }
+    private InputTrackingState _TrackingState;
     private void SendOSCMessage()
     {
         InitTracker.oscClient.Send(OSCAddress + "/Position", transform.position.x, transform.position.y, transform.position.z);
         InitTracker.oscClient.Send(OSCAddress + "/Rotation", transform.rotation.w, transform.rotation.x, transform.rotation.y, transform.rotation.z);
         InitTracker.oscClient.Send(OSCAddress + "/Battery", trackerManager.GetTrackerBatteryLife(TrackerId));
     }
+
+    void Update()
+    {
+        if (trackerManager.IsTrackerConnected(TrackerId))
+        {
+            ShowInfo = true;
+
+            transform.position = trackerManager.GetTrackerPosition(TrackerId);
+            transform.rotation = trackerManager.GetTrackerRotation(TrackerId);
+
+            BatteryValue = trackerManager.GetTrackerBatteryLife(TrackerId);
+            InputTrackingState TS;
+            trackerManager.GetTrackerTrackingState(TrackerId, out TS);
+
+            TrackingState = TS;
+
+            SendOSCMessage();
+        }
+        else
+            ShowInfo = false;
+    }
+
 }
